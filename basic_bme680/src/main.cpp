2c8b984da7a492e9113235e5808f7753f97228fa
adrian.pop <pop.adrian61@gmail.com>
2019-06-06 13:29:35 +0300

    - update basic_bme680, use PlatformIO - working basic bme680 measurements read

--- basic_bme680/src/main.cpp
+++ basic_bme680/src/main.cpp
@@ -8,52 +8,59 @@ void errLeds(void);
 Bsec iaqSensor;
 
 String output;
-char sendBuffer[200];
 
 // Entry point for the example
 void setup(void)
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  delay(5000);
+  Wire.begin();
 
   iaqSensor.begin(BME680_I2C_ADDR_SECONDARY, Wire);
   output = "\nBSEC library version " + String(iaqSensor.version.major) + "." + String(iaqSensor.version.minor) + "." + String(iaqSensor.version.major_bugfix) + "." + String(iaqSensor.version.minor_bugfix);
   Serial.println(output);
   checkIaqSensorStatus();
 
-  bsec_virtual_sensor_t sensorList[8] = {
+  bsec_virtual_sensor_t sensorList[10] = {
     BSEC_OUTPUT_RAW_TEMPERATURE,
     BSEC_OUTPUT_RAW_PRESSURE,
     BSEC_OUTPUT_RAW_HUMIDITY,
     BSEC_OUTPUT_RAW_GAS,
     BSEC_OUTPUT_IAQ,
+    BSEC_OUTPUT_STATIC_IAQ,
+    BSEC_OUTPUT_CO2_EQUIVALENT,
+    BSEC_OUTPUT_BREATH_VOC_EQUIVALENT,
     BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_TEMPERATURE,
     BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_HUMIDITY,
   };
 
-  iaqSensor.updateSubscription(sensorList, 8, BSEC_SAMPLE_RATE_LP);
+  iaqSensor.updateSubscription(sensorList, 10, BSEC_SAMPLE_RATE_LP);
   checkIaqSensorStatus();
+
+  // Print the header
+  output = "Timestamp [ms], raw temperature [°C], pressure [hPa], raw relative humidity [%], gas [Ohm], IAQ, IAQ accuracy, temperature [°C], relative humidity [%], Static IAQ, CO2 equivalent, breath VOC equivalent";
+  Serial.println(output);
 }
 
 // Function that is looped forever
 void loop(void)
 {
-  if (iaqSensor.run()) // If new data is available
-  {
-    snprintf(sendBuffer, sizeof(sendBuffer),
-                        "{\"IAQ_Accuracy\": \"%d\",\"IAQ\":\"%.2f\""
-                        ",\"Temperature\": \"%.2f\",\"Humidity\": \"%.2f\",\"Pressure\": \"%.2f\""
-                        ",\"Gas\": \"%.0f\""
-                        ",\"Status\": \"%d\"}",
-                        iaqSensor.iaqAccuracy, iaqSensor.iaqEstimate,
-                        iaqSensor.temperature, iaqSensor.humidity, iaqSensor.pressure / 100,
-                        iaqSensor.gasResistance,
-                        iaqSensor.status);
-    strcat(sendBuffer, "\r\n");
-    Serial.println(sendBuffer);
-    memset(sendBuffer, 0, sizeof(sendBuffer));
-  }
-  else
-  {
+  unsigned long time_trigger = millis();
+  if (iaqSensor.run()) { // If new data is available
+    output = String(time_trigger);
+    output += ", " + String(iaqSensor.rawTemperature);
+    output += ", " + String(iaqSensor.pressure);
+    output += ", " + String(iaqSensor.rawHumidity);
+    output += ", " + String(iaqSensor.gasResistance);
+    output += ", " + String(iaqSensor.iaq);
+    output += ", " + String(iaqSensor.iaqAccuracy);
+    output += ", " + String(iaqSensor.temperature);
+    output += ", " + String(iaqSensor.humidity);
+    output += ", " + String(iaqSensor.staticIaq);
+    output += ", " + String(iaqSensor.co2Equivalent);
+    output += ", " + String(iaqSensor.breathVocEquivalent);
+    Serial.println(output);
+  } else {
     checkIaqSensorStatus();
   }
 }
@@ -63,7 +70,7 @@ void checkIaqSensorStatus(void)
 {
   if (iaqSensor.status != BSEC_OK) {
     if (iaqSensor.status < BSEC_OK) {
-      output = "1 BSEC error code : " + String(iaqSensor.status);
+      output = "BSEC error code : " + String(iaqSensor.status);
       Serial.println(output);
       for (;;)
         errLeds(); /* Halt in case of failure */
@@ -75,7 +82,7 @@ void checkIaqSensorStatus(void)
 
   if (iaqSensor.bme680Status != BME680_OK) {
     if (iaqSensor.bme680Status < BME680_OK) {
-      output = "2 BME680 error code : " + String(iaqSensor.bme680Status);
+      output = "BME680 error code : " + String(iaqSensor.bme680Status);
       Serial.println(output);
       for (;;)
         errLeds(); /* Halt in case of failure */
@@ -90,7 +97,7 @@ void errLeds(void)
 {
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
+  delay(100);
   digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
+  delay(100);
 }
